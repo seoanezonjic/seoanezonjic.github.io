1af7960b11153fdb99d679a996dcc5e0ebd5b14a
seoanezonjic <darklordsone@hotmail.com>
2025-12-19 10:17:30 +0100

    Created docs

---
+++ get_files.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+cp ~/dev_py/py_cmdtabs/tests/cli_example_report/Report.html docs/py_docs/cmdtabs.html
+cp ~/dev_py/py_semtools/tests/cli_examples/Report.html docs/py_docs/semtools.html
