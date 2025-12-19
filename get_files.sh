907dc04b6b24cde7123618e2f577a52cce37ffd7
seoanezonjic <darklordsone@hotmail.com>
2025-12-19 12:09:08 +0100

    Added grimorio

--- get_files.sh
+++ get_files.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 
+cp ~/dev_latex/grimorio/main.pdf docs/grimorio.pdf
+
 cp ~/dev_py/py_cmdtabs/tests/cli_example_report/Report.html docs/py_docs/cmdtabs.html
 cp ~/dev_py/py_semtools/tests/cli_examples/Report.html docs/py_docs/semtools.html
